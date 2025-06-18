4839a4e1ad296a658f39de4134b846bdf393c38f
martijn <martijn@itsallcode.nl>
2025-06-18 09:44:15 +0200

    feat: cases

--- apps/simple.py
+++ apps/simple.py
@@ -1,6 +1,19 @@
 from etl.extractors import FileExtractor
 
 
+def feature():
+    extractor = FileExtractor()
+    ds = extractor.extract('./apps/simple.json')
+
+    ds['total'] = ds['price'] * ds['quantity']
+    return ds
+
+
+def extra():
+    ds = feature()
+    print(sum(ds['total']))
+
+
 def main():
     extractor = FileExtractor()
     ds = extractor.extract('./apps/simple.json')
